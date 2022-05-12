4919561f4caf5cd544bdb28616df920815160de1
thehuy2512 <thehuy.2pro@gmail.com>
2022-05-12 13:50:58 +0700

    them bang help contact

---
+++ src/api/help-contact/routes/help-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * help-contact router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::help-contact.help-contact');
